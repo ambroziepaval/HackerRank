4e15d2d539247e694fdeccfeebc0a220309439a9
Paval, Ambrozie <ambroziepaval@gmail.com>
2020-07-27 20:40:40 +0300

    Counting Valleys - 15.00

---
+++ src/ambroziepaval/hackerrank/warmup/CountingValleys.java
@@ -0,0 +1,36 @@
+package ambroziepaval.hackerrank.warmup;
+
+public class CountingValleys {
+
+    static int countingValleys(int n, String s) {
+
+        int valleyCount = 0;
+
+        boolean belowZero = false;
+        int altitude = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == 'D') {
+                altitude--;
+                belowZero = altitude < 0;
+            } else {
+                altitude++;
+                if (altitude == 0 && belowZero) {
+                    valleyCount++;
+                }
+            }
+        }
+
+        return valleyCount;
+    }
+
+    public static void main(String[] args) {
+
+        int n = 8;
+//        String s = "DDUUUUDD";
+        String s = "UDDDUDUU";
+
+        System.out.println(CountingValleys.countingValleys(n, s));
+    }
+}
